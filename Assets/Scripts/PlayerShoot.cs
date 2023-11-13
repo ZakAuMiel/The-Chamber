dfe006c12088a52abb356dea0193642e7da300d8
Zak Oubbéa <zakaria.oubbea@actimage.eu>
2023-11-13 20:52:34 +0100

    Weapon System Start!

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Mirror;
+
+public class PlayerShoot : NetworkBehaviour
+{
+
+    public PlayerWeapon weapon;
+
+    [SerializeField]
+    private Camera cam;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            Debug.LogError("PlayerShoot: No camera referenced!");
+            this.enabled = false;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, weapon.range))
+        {
+            Debug.Log("We hit " + hit.collider.name);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerWeapon.cs
@@ -0,0 +1,9 @@
+[System.Serializable]
+public class PlayerWeapon 
+{
+    public string name = "Glock";
+
+    public float damage = 10f;
+    public float range = 100f;
+
+}
